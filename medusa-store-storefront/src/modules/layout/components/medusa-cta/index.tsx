0b525bb8106c4d9558c90a27137560947057b046
inerttila <inert.etila@gmail.com>
2025-02-04 15:15:05 +0100

    add test things

--- medusa-store-storefront/src/modules/layout/components/medusa-cta/index.tsx
+++ medusa-store-storefront/src/modules/layout/components/medusa-cta/index.tsx
@@ -6,13 +6,24 @@ import NextJs from "../../../common/icons/nextjs"
 const MedusaCTA = () => {
   return (
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
-      Powered by
+      Powered by Inerttila
       <a href="https://www.medusajs.com" target="_blank" rel="noreferrer">
         <Medusa fill="#9ca3af" className="fill-[#9ca3af]" />
       </a>
       &
       <a href="https://nextjs.org" target="_blank" rel="noreferrer">
         <NextJs fill="#9ca3af" />
+      </a>
+      &
+      <a href="https://github.com/inerttila" target="_blank" rel="noreferrer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="#9ca3af"
+          className="w-5 h-5"
+        >
+          <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577v-2.17c-3.338.726-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.756-1.333-1.756-1.09-.744.083-.729.083-.729 1.205.084 1.84 1.237 1.84 1.237 1.07 1.835 2.807 1.305 3.492.997.108-.774.42-1.305.763-1.605-2.665-.305-5.467-1.332-5.467-5.93 0-1.31.468-2.38 1.236-3.22-.124-.303-.536-1.523.117-3.176 0 0 1.008-.322 3.3 1.23.96-.267 1.98-.4 3-.405 1.02.005 2.04.138 3 .405 2.29-1.552 3.297-1.23 3.297-1.23.654 1.653.242 2.873.118 3.176.77.84 1.236 1.91 1.236 3.22 0 4.61-2.807 5.62-5.48 5.92.43.37.823 1.1.823 2.22v3.293c0 .32.192.694.8.577C20.565 21.8 24 17.3 24 12c0-6.63-5.37-12-12-12z" />
+        </svg>
       </a>
     </Text>
   )
--- medusa-store-storefront/src/modules/layout/templates/footer/index.tsx
+++ medusa-store-storefront/src/modules/layout/templates/footer/index.tsx
@@ -147,7 +147,7 @@ export default async function Footer() {
         </div>
         <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
           <Text className="txt-compact-small">
-            © {new Date().getFullYear()} Medusa Store. All rights reserved.
+            © {new Date().getFullYear()} Inert Store. All rights reserved.
           </Text>
           <MedusaCTA />
         </div>
